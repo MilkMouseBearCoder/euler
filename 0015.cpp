52907c2aa7a096f90018aea2c3407fda1300f34e
Leon Bezuidenhout <abadhideout@gmail.com>
2013-08-26 21:40:23 +0200

    Got the answer, but it does not work with large grids. OVERFLOW

--- 0015.cpp
+++ 0015.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 unsigned long LatticePaths(unsigned long, unsigned long);
 unsigned long SigmaSum(unsigned long);
-
+unsigned long Factorial(unsigned long);
+unsigned long Factorial(unsigned long, unsigned long);
 int main()
 {
 	cout << LatticePaths(3, 3);
-
+	//cout << Factorial(20);
 	return 0;
 }
 
@@ -16,10 +17,29 @@ unsigned long LatticePaths(unsigned long x, unsigned long y)
 {
 	unsigned long totalSteps = x + y;
 
-	return SigmaSum(totalSteps) / (SigmaSum(totalSteps - x) * SigmaSum(x));
+	return Factorial(totalSteps - x, totalSteps) / Factorial(x);
 }
 
-unsigned long SigmaSum(unsigned long n)
+unsigned long Factorial(unsigned long n1, unsigned long n2)
 {
-	return (n * (n + 1)) / 2;
+	unsigned long result = 1;
+
+	for(unsigned long i = n2; i > n1; i--)
+	{
+		result*=i;
+	}
+
+	return result;
+}
+
+unsigned long Factorial(unsigned long n)
+{
+	unsigned long result = 1;
+
+	for(unsigned long i = n; i > 0; i--)
+	{
+		result*=i;
+	}
+
+	return result;
 }

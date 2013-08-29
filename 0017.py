7004bd4fc9d56f852ae42e891b11455a27b2f226
leon <leon@rightshift.biz>
2013-08-29 15:33:28 +0200

    Improved the answer for 17

--- 0017.py
+++ 0017.py
@@ -3,21 +3,16 @@
 
 class NumberConverter(object):
     def __init__(self):
-        self.ONES = ['one', 'two', 'three', 'four', 'five',
-                     'six', 'seven', 'eight', 'nine']
-        self.TENS = ['ten', 'twenty', 'thirty', 'forty', 'fifty',
-                     'sixty', 'seventy', 'eighty', 'ninety']
-        self.HUNDREDS = '{} hundred'
-        self.THOUSANDS = '{} thousand'
-
-    def __convert_ones_to_words__(self, n):
-        return self.ONES[n -1]
-
-    def __convert_tens_to_words__(self, n):
-        return self.TENS[n - 1]
-
-    def __convert_teens_to_words__(self, n):
-        teens = {
+        self.TRANSLATE = {
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six',
+            7: 'seven',
+            8: 'eight',
+            9: 'nine',
             10: 'ten',
             11: 'eleven',
             12: 'twelve',
@@ -27,60 +22,57 @@ class NumberConverter(object):
             16: 'sixteen',
             17: 'seventeen',
             18: 'eighteen',
-            19: 'nineteen'
+            19: 'nineteen',
+            20: 'twenty',
+            30: 'thirty',
+            40: 'forty',
+            50: 'fifty',
+            60: 'sixty',
+            70: 'seventy',
+            80: 'eighty',
+            90: 'ninety',
+            100: 'hundred',
+            1000: 'thousand'
         }
-
-        return teens[n]
-        
-
-    def __convert_hundreds_to_words__(self, n):
-        return self.HUNDREDS.format(self.__convert_ones_to_words__(n))
-
-    def __convert_thousands_to_words__(self, n):
-        return self.THOUSANDS.format(self.__convert_ones_to_words__(n))
 
     def __and__(self, s):
         if s != '':
-            s+= ' and '
+            s += ' and '
         return s
 
     def __space__(self, s):
         if s != '':
-            s+= ' '
+            s += ' '
         return s
-
 
     def to_words(self, n):
         result = ''
 
         ones = n % 10
-        tens = (n % 100)  - ones
+        tens = (n % 100) - ones
         hundreds = (n % 1000) - tens - ones
         thousands = n - hundreds - tens - ones
 
         if thousands > 0:
-            result+=self.__convert_thousands_to_words__(thousands / 1000)
-
+            result += self.TRANSLATE[thousands / 1000] + ' ' \
+                + self.TRANSLATE[1000]
         if hundreds > 0:
             result = self.__space__(result)
-            word = self.__convert_hundreds_to_words__(hundreds / 100)
-#            if thousands == 0:
-#                word = word.replace('one', 'a')
-            result+=word
-
+            result += self.TRANSLATE[hundreds / 100] + ' ' \
+                + self.TRANSLATE[100]
         if tens == 10:
             result = self.__and__(result)
-            result+=self.__convert_teens_to_words__(tens + ones)
+            result += self.TRANSLATE[tens + ones]
         elif tens > 10:
             result = self.__and__(result)
-            result+=self.__convert_tens_to_words__(tens / 10)
-
+            result += self.TRANSLATE[tens]
+        
         if tens != 10 and ones > 0:
             if tens == 0:
                 result = self.__and__(result)
             else:
                 result = self.__space__(result)
-            result+=self.__convert_ones_to_words__(ones)
+            result += self.TRANSLATE[ones]
 
         return result
 
@@ -89,14 +81,14 @@ if __name__ == '__main__':
     converter = NumberConverter()
 
     result = ''
-    totalLength = 0;
+    totalLength = 0
 
     while n < 1001:
-        words = converter.to_words(n);
+        words = converter.to_words(n)
         print words
         words = words.replace(' ', '').replace('-', '')
-        totalLength+=len(words)
-        n+=1
+        totalLength += len(words)
+        n += 1
 
     result = result.replace(' ', '')
     print totalLength
